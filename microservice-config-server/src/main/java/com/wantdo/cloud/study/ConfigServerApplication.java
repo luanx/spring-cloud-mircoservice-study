3f2484a3a3ccd0cb57355fbbd961f43ff23e174e
luanx <luanx@wantdo.com>
2017-09-29 16:17:21 +0800

    Initial commit

---
+++ microservice-config-server/src/main/java/com/wantdo/cloud/study/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.wantdo.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author: luanx
+ * @Email: luanx@wantdo.com
+ * @Date: 2017-09-29
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
