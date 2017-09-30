12301f0395d79f7a505769792ddee534751ea812
luanx <luanx@wantdo.com>
2017-09-30 09:24:53 +0800

    add spring cloud config file

---
+++ microservice-config-client/src/main/java/com/wantdo/cloud/study/controller/ConfigClientApplication.java
@@ -0,0 +1,17 @@
+package com.wantdo.cloud.study.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: luanx
+ * @Email: luanx@wantdo.com
+ * @Date: 2017-09-29
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
